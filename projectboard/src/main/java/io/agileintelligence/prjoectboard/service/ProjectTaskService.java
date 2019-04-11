98a73757e38ff0238468d59f2e1903d48f1884c4
Keval Shah <keval.shah@scandiacompany.net>
2019-04-11 11:34:09 -0600

    First commit

---
+++ projectboard/src/main/java/io/agileintelligence/prjoectboard/service/ProjectTaskService.java
@@ -0,0 +1,35 @@
+package io.agileintelligence.prjoectboard.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.agileintelligence.prjoectboard.domain.ProjectTask;
+import io.agileintelligence.prjoectboard.repository.ProjectTaskRepository;
+
+@Service
+public class ProjectTaskService {
+
+	@Autowired
+	private ProjectTaskRepository projectTaskRepository;
+	
+	public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask) {
+		
+		if(projectTask.getStatus() == null || projectTask.getStatus() == "") {
+			projectTask.setStatus("TO_DO");
+		}
+		return projectTaskRepository.save(projectTask);
+	}
+	
+	public Iterable<ProjectTask> finalAll(){
+		return projectTaskRepository.findAll();
+	}
+	
+	public ProjectTask findById(Long id) {
+		return projectTaskRepository.getById(id);
+	}
+	
+	public void delete(Long id) {
+		ProjectTask projectTask = findById(id);
+		projectTaskRepository.delete(projectTask);		
+	}
+}
